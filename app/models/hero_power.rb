aaf75625fc5c20436fd58889397f4c75bb849457
Ashley <ashleynjeri7@gmail.com>
2022-12-05 14:06:27 +0300

    Added Validations

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -2,5 +2,5 @@ class HeroPower < ApplicationRecord
     belongs_to :hero
     belongs_to :power
 
-    validates :strength, inclusion: {in: %w(Strong Weak Average), "%{value} is invalid! Strength can only be strong, weak or average."}
+    validates :strength, inclusion: { in: %w(Strong Weak Average), message: "%{value} is invalid! Strength can only be strong, weak or average."}
 end
