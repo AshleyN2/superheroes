b5c68268be43fef4e513987fd8f0215a655e3b6d
Ashley <ashleynjeri7@gmail.com>
2022-12-05 15:21:16 +0300

    Added Routes

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,5 +1,6 @@
 class HeroPowersController < ApplicationController
 
+    # POST /hero_powers
     def create 
         hero_power = HeroPower.create!(hero_power_params)
          render json: hero_power.hero, status: :created, serializer: HeroPowerSerializer 
