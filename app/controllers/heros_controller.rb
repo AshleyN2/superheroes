f4b605276caba3f113c4a29c3dc64e676edbb084
Ashley <ashleynjeri7@gmail.com>
2022-12-05 15:13:22 +0300

    Added Actions to Controllers

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,13 @@
 class HerosController < ApplicationController
+    
+    def index 
+        heroes = Hero.all
+        render json: heroes,status: :ok
+    end
+
+    def show
+        hero = Hero.find(params[:id])
+            render json: hero,status: :ok, serializer: HeroPowerSerializer
+    end
+
 end
